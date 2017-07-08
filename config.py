d57a75b8642b259965af68d67c57fcd48c5c6bbe
parassharmaa <paraazz@outlook.com>
2017-07-09 01:35:09 +0530

    [UPDATE] added app.json

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+	return '''Hi {}, thanks for following us!
+	Find more resources for Soccer COaches at TacticalPedia.com'''.format(name)
 
 scheduler_time = 15 #in minutes
 
-tw_username = "paraazz" #change this to yours
+tw_username = "tacticalpedia" #change this to yours
