96bc686a6d030afba80d0a16f52d486194c8c077
QurQuma <34918414+QurQuma@users.noreply.github.com>
2018-03-16 13:52:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Hi {}, thanks for following us!
-	Find more resources and support for Soccer Coaches at TacticalPedia.com'''.format(name)
+	return '''Hi {}, thanks for following!
+	Find more resources and support for Soccer Coaches at TacticalPedia
+	tacticalpedia.com'''.format(name)
 
 scheduler_time = 15 #in minutes
 
